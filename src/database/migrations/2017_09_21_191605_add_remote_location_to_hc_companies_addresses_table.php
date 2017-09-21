5bc99d9b8b8197e8839536e14a9ea96e508d024b
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-09-21 22:31:35 +0300

    Added migration...

---
+++ src/database/migrations/2017_09_21_191605_add_remote_location_to_hc_companies_addresses_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRemoteLocationToHcCompaniesAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('hc_companies_addresses', function (Blueprint $table) {
+            $table->enum('remote_location', ['0', '1'])->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hc_companies_addresses', function (Blueprint $table) {
+            $table->dropColumn('remote_location');
+        });
+    }
+}
