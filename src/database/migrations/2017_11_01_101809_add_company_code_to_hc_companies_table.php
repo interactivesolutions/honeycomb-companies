4895567a7f80663ae26cb168be588b7ce5aa6e58
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-11-01 13:19:17 +0200

    Added migration for new field

---
+++ src/database/migrations/2017_11_01_101809_add_company_code_to_hc_companies_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use interactivesolutions\honeycombcompanies\app\models\HCCompanies;
+
+class AddCompanyCodeToHcCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     * @return void
+     * @throws Exception
+     */
+    public function up()
+    {
+        if (HCCompanies::withTrashed()->count() > 0)
+            throw new Exception('hc_companies needs to be empty!');
+
+        Schema::table('hc_companies', function (Blueprint $table) {
+            $table->string('company_code', 36)->index()->nullable();
+
+            $table->unique(['company_code', 'country_id'], 'unique_company_in_country');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('hc_companies', function (Blueprint $table) {
+
+            $table->dropUnique('unique_company_in_country');
+
+            $table->dropColumn('company_code');
+        });
+    }
+}
